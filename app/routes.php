1d015d6ac579c831b0fe0aa8e59a1a21073d767f
JB LM <lemetayer.jb@gmail.com>
2024-12-24 12:59:36 +0100

    routes de listing des dossiers et upload d'annexes

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Controllers\Main;
+use Controllers\Record;
 
 $f3 = Base::instance();
 
@@ -8,3 +9,6 @@ $f3->route('GET @index: /', Main::class.'->index');
 $f3->route('GET @setup: /setup', Main::class.'->setup');
 $f3->route('GET @form: /form', Main::class.'->form');
 $f3->route('POST @fill: /fill', Main::class.'->fill');
+
+$f3->route('GET @records: /records', Record::class.'->index');
+$f3->route('GET|POST @record_attchment: /record/@record/submission/@submission/attachment', Record::class.'->attachment');
