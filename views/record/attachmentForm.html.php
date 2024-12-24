7f742df0c5c09958224e269e1583e1282d6f0b3a
JB LM <lemetayer.jb@gmail.com>
2024-12-24 12:55:08 +0100

    formulaire d'upload d'annexe pour les depots de dossier

---
+++ views/record/attachmentForm.html.php
@@ -0,0 +1,15 @@
+<h1>Attachment</h1>
+
+<form method="POST" class="row" enctype="multipart/form-data" action="/record/<?php echo $submission->record->name ?>/submission/<?php echo $submission->name ?>/attachment">
+<p class="text-center"><?php echo $this->raw($submission->getAttachmentNeeded()); ?></p>
+<div class="col-6 offset-3 mt-3 justify-content-center">
+  <?php if (isset($uploadError)): ?><p class="text-center text-danger">Une erreur est survenue</p><?php endif; ?>
+  <div class="mb-3 text-center">
+    <label class="form-label" for="attachment">Sélectionner un fichier</label>
+    <input type="file" class="form-control form-control-lg" name="attachment" />
+  </div>
+  <div class="text-end">
+    <button type="submit" class="btn btn-primary">Joindre</button>
+  </div>
+</div>
+</form>
