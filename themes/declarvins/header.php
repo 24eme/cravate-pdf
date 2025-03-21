60bc57864a907f3417f87f22a0558d3cb45a1357
JB LM <lemetayer.jb@gmail.com>
2025-03-21 11:20:44 +0100

    adaptation a la refacto

--- themes/declarvins/header.php
+++ themes/declarvins/header.php
@@ -1,5 +1,5 @@
 <?php if ($headerLink = $config->get('header_link')): ?>
 <div class="container themeHeaderContainer">
-  <?php echo file_get_contents(sprintf($headerLink, $_SESSION['etablissement_id'], $_SESSION['is_admin'])); ?>
+  <?php echo file_get_contents(sprintf($headerLink, $user->getUserId(), $user->isAdmin())); ?>
 </div>
 <?php endif; ?>
