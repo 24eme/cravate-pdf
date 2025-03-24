bd3e1860d811f08aa5a27288b3317187c008a2b1
Vincent LAURENT <vince.laurent@gmail.com>
2025-03-24 12:33:01 +0100

    Mise en forme d'un item de l'attachment, clickable

--- views/procedure/attachment.html.php
+++ views/procedure/attachment.html.php
@@ -16,38 +16,10 @@
   <div class="row justify-content-center">
     <div class="col-6">
     <h3 class="mt-3">Joindre une pièce complémentaire</h3>
-    <ul class="list-group mt-4">
+    <ul id="attachment-list" class="list-group mt-4">
     <?php foreach($submission->getAttachmentsNeeded() as $attachment): ?>
-        <li class="list-group-item attachment-item" <?php if($submission->getAttachmentByName($attachment['filename'])): ?>data-existing="true"<?php endif; ?>><i class="bi bi-square"></i> <label><?php echo $attachment['label'] ?></label>
+        <li class="list-group-item attachment-item" style="cursor: pointer;"   <?php if($submission->getAttachmentByName($attachment['filename'])): ?>data-existing="true"<?php endif; ?>><i class="bi bi-square me-2"></i> <label style="cursor: pointer;"><?php echo $attachment['label'] ?></label>
         <button id="<?php echo $attachment['filename'] ?>" type="button" class="btn btn-light btn-add float-end" data-bs-toggle="modal" data-bs-target="#<?php echo $attachment['filename'] ?>_modal">Ajouter</button>
-        <div id="<?php echo $attachment['filename'] ?>_modal" class="modal" tabindex="-1">
-          <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title"><?php echo $attachment['label'] ?></h5>
-                <button id="<?php echo $attachment['filename'] ?>_close" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <?php if(isset($submission->json->annexes->{$attachment['filename']})): ?>
-                <p>Choisissez parmi cette liste :</p>
-                <div class="card">
-                  <div class="card-header">
-                    Liste des pièces existantes
-                  </div>
-                  <div id="<?php echo $attachment['filename'] ?>_list" class="list-group list-group-flush liste-existing">
-                  <?php foreach($submission->json->annexes->{$attachment['filename']} as $label => $url): ?>
-                      <a href="<?php echo $url ?>" class="list-group-item list-group-item-action small" target="_blank"><i class="bi bi-file-earmark"></i> <?php echo $label ?></a>
-                  <?php endforeach; ?>
-                  </div>
-                  </div>
-                  <p class="mt-3"><strong>Ou</strong></p>
-                  <?php endif; ?>
-                  <p>Choisissez un fichier sur votre ordinateur :</p>
-                  <input id="<?php echo $attachment['filename'] ?>_file" type="file" class="form-control" name="<?php echo $attachment['filename'] ?>" />
-              </div>
-            </div>
-          </div>
-        </div>
       </li>
     <?php endforeach; ?>
     </ul>
@@ -56,6 +28,36 @@
     </div>
     </div>
   </div>
+  <?php foreach($submission->getAttachmentsNeeded() as $attachment): ?>
+    <div id="<?php echo $attachment['filename'] ?>_modal" class="modal" tabindex="-1">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title"><?php echo $attachment['label'] ?></h5>
+            <button id="<?php echo $attachment['filename'] ?>_close" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <?php if(isset($submission->json->annexes->{$attachment['filename']})): ?>
+            <p>Choisissez parmi cette liste :</p>
+            <div class="card">
+              <div class="card-header">
+                Liste des pièces existantes
+              </div>
+              <div id="<?php echo $attachment['filename'] ?>_list" class="list-group list-group-flush liste-existing">
+              <?php foreach($submission->json->annexes->{$attachment['filename']} as $label => $url): ?>
+                  <a href="<?php echo $url ?>" class="list-group-item list-group-item-action small" target="_blank"><i class="bi bi-file-earmark"></i> <?php echo $label ?></a>
+              <?php endforeach; ?>
+              </div>
+              </div>
+              <p class="mt-3"><strong>Ou</strong></p>
+              <?php endif; ?>
+              <p>Choisissez un fichier sur votre ordinateur :</p>
+              <input id="<?php echo $attachment['filename'] ?>_file" type="file" class="form-control" name="<?php echo $attachment['filename'] ?>" />
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php endforeach; ?>
   <script>
     document.querySelectorAll('.btn-add').forEach(function (item) {
       item.addEventListener('click', function (e) {
@@ -82,6 +84,12 @@
         stateListe();
         return false;
       })
+    });
+
+    document.querySelector('#attachment-list').addEventListener('click', function (e) {
+      if(e.target.closest('.attachment-item')) {
+        e.target.closest('.attachment-item').querySelector('button').click();
+      }
     });
 
     function stateListe() {
