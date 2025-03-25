57588e7f843a7bfa1876b9b03e1897eda2fbf33b
Vincent LAURENT <vince.laurent@gmail.com>
2025-03-25 15:03:18 +0100

    Bon namespace

--- themes/declarvins/header.php
+++ themes/declarvins/header.php
@@ -1,5 +1,5 @@
 <?php if ($headerLink = $config->get('header_link')): ?>
 <div class="container themeHeaderContainer">
-  <?php echo file_get_contents(sprintf($headerLink, User::instance()->getUserId(), User::instance()->isAdmin())); ?>
+  <?php echo file_get_contents(sprintf($headerLink, \User\User::instance()->getUserId(), \User\User::instance()->isAdmin())); ?>
 </div>
 <?php endif; ?>
