2953f12e2b1755b4330fe7b9024aa1a2171ab317
Gabriel Poma <gpoma@24eme.fr>
2025-04-01 10:43:28 +0200

    config hostname
    workaround pour le mail de dépot. URLBASE et alias() contiennent
    /cravate-pdf/ dans le lien généré. Il était donc en double.

--- app/common.php
+++ app/common.php
@@ -12,6 +12,7 @@ if(getenv("DEBUG")) {
 $f3->set('ROOT', __DIR__.'/../');
 $f3->set('UI', '../views/');
 $f3->set('URLBASE', Config::getInstance()->getUrlbase());
+$f3->set('HOSTNAME', Config::getInstance()->getHostname());
 $f3->set('VERSION', Config::getInstance()->getCommit());
 $f3->set('config', Config::getInstance());
 
